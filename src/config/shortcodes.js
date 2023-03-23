1872099f8d6fdc3a4d02baa93c3a812fad2f5bf4
By-Chris <chrisbyars@rebelinteractivegroup.com>
2023-03-23 15:41:26 -0400

    Trying to add custom code

--- src/config/shortcodes.js
+++ src/config/shortcodes.js
@@ -53,3 +53,11 @@ module.exports = {
     eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
   }
 }
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.setFrontMatterParsingOptions({
+    excerpt: true,
+    // Optional, default is "---"
+    excerpt_separator: "<!-- excerpt -->"
+  });
+};

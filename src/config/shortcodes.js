41bf97ff742a67e19b3f5d587436aa836eb54998
By-Chris <chrisbyars@rebelinteractivegroup.com>
2023-03-23 15:42:34 -0400

    Reverting

--- src/config/shortcodes.js
+++ src/config/shortcodes.js
@@ -53,11 +53,3 @@ module.exports = {
     eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
   }
 }
-
-module.exports = function(eleventyConfig) {
-  eleventyConfig.setFrontMatterParsingOptions({
-    excerpt: true,
-    // Optional, default is "---"
-    excerpt_separator: "<!-- excerpt -->"
-  });
-};
